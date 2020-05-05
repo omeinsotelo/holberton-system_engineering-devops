dab17a43e2a0a161f89c685a6986a9a12596a7ae
omeinsotelo <omeinsotelo@gmail.com>
2020-05-04 22:43:40 -0500

    upload task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,12 +10,12 @@ if __name__ == '__main__':
     # Url
     api = 'https://jsonplaceholder.typicode.com/users/'
     users = requests.get(api).json()
-    data = {}
+    json_format = {}
     for user in users:
         # User request of parameter (id)
         todo = requests.get(api + '{}/todos'.format(user['id'])).json()
-        # List of list of all data to export
-        data[user['id']] = [{
+        # List of list of all json_format to export
+        json_format[user['id']] = [{
             'task': task.get('title'),
             'completed': task.get('completed'),
             'username': user['username']
@@ -23,4 +23,4 @@ if __name__ == '__main__':
 
     # Export and write in format .json
     with open('todo_all_employees.json', 'w') as file:
-        json.dump(data, file)
+        json.dump(json_format, file)
