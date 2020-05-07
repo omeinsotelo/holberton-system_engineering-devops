8322dcf1b23b721b1f279cd7317cb1c9121dfedc
omeinsotelo <omeinsotelo@gmail.com>
2020-05-07 18:48:00 -0500

    upload 0 and readme

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Number of subscribers
+    """
+    headers = {"User-Agent": "paulasv"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        return req.json().get("data").get("subscribers")
+    except Exception:
+        return 0
