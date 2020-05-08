d42b38ac7b197089bc30835d5474c0d9cd467f0c
omeinsotelo <omeinsotelo@gmail.com>
2020-05-07 19:32:26 -0500

    upload 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Prints titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    first 10 hot posts listed for a given subreddit
+    """
+    headers = {"User-Agent": "paulasv"}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    req = requests.get(url, headers=headers, allow_redirects=False)
+
+    try:
+        if (req.status_code != 200):
+            raise Exception()
+        json_data = req.json()
+        for item in json_data.get("data")["children"]:
+            print(item.get("data")["title"])
+    except Exception:
+        print("None")
